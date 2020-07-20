ba5a7e4ee8d5d65d948abefb39c56da9d19b439c
Chang Shu <changshu.qd@gmail.com>
2020-07-20 08:35:53 +0000

    update the Role 'Administrator' to 'admin' to keep same with the default 'admin'

--- hasura_mam/seeds/001_staff.sql
+++ hasura_mam/seeds/001_staff.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.staff (
     first_name, last_name, gender, mobile, email, password, is_active, joined_at, default_role
 ) VALUES
-    ('畅', '舒', 'M', '18563950872', 'changshu.qd@gmail.com', 'sc', 'true', '2018-04-30 00:00:00+08', 'Administrator');
+    ('畅', '舒', 'M', '18563950872', 'changshu.qd@gmail.com', 'sc', 'true', '2018-04-30 00:00:00+08', 'admin');
 
 INSERT INTO public.staff_role(
     staff, role, created_by

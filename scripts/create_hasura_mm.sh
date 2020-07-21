ebeece79f94042e7ef69e1efa3986f6d21a1e36a
Chang Shu <changshu.qd@gmail.com>
2020-07-21 13:42:06 +0000

    "--admin-secret" and position arg $1 for migration name

--- scripts/create_hasura_mm.sh
+++ scripts/create_hasura_mm.sh
@@ -8,16 +8,16 @@ else
 fi
 echo $(pwd)
 echo "create migration ..."
-hasura migrate create --from-server --endpoint http://192.168.1.182:8080
+hasura migrate create $1 --from-server --endpoint http://192.168.1.182:8080 --admin-secret tbqIwPZ_7rw
 # find the version of the new created migration
-for f in $(ls -c hasura_mam/migrations); do
+for f in $(ls -c migrations); do
   version=$(echo $f | cut -d'_' -f 1)
   break
 done
-hasura migrate apply --endpoint=http://192.168.1.182:8080 --version $version --skip-execution
+hasura migrate apply --endpoint=http://192.168.1.182:8080 --version $version --skip-execution --admin-secret tbqIwPZ_7rw
 
 echo "create metadata ..."
-hasura metadata export --endpoint http://192.168.1.182:8080
+hasura metadata export --endpoint http://192.168.1.182:8080  --admin-secret tbqIwPZ_7rw
 
 cd ..
 echo "done!"

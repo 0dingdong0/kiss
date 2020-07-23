5c314cb3009f8a7c80420b25e0609817ecad6732
Chang Shu <changshu.qd@gmail.com>
2020-07-23 17:57:01 +0000

    skip check update

--- scripts/run_hasura_console.sh
+++ scripts/run_hasura_console.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hasura console --address 192.168.1.182 --admin-secret tbqIwPZ_7rw --no-browser --project hasura_mam
+hasura console --address 192.168.1.182 --admin-secret tbqIwPZ_7rw --no-browser --skip-update-check --project hasura_mam

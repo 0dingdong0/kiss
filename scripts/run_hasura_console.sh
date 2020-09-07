ff1b5e697209d9393865dc60c9e76c5c4a1e909c
Chang Shu <changshu.qd@gmail.com>
2020-09-07 02:31:31 +0000

    update hasura console port

--- scripts/run_hasura_console.sh
+++ scripts/run_hasura_console.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-hasura console --address 192.168.1.182 --admin-secret tbqIwPZ_7rw --no-browser --project hasura_mam
+hasura console --address 192.168.1.182 --console-port 8080 --admin-secret tbqIwPZ_7rw --no-browser --project hasura_mam
 
-=======
-hasura console --address 192.168.1.182 --admin-secret tbqIwPZ_7rw --no-browser --skip-update-check --project hasura_mam
->>>>>>> 09ae0799ceababbc9d4287247501364d00226330
